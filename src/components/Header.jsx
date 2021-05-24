32ebec59493f9e472d9a19dc8f67d5e61f702d99
Samuel Ševčik <s.sevcik.st@spseiostrava.cz>
2021-05-24 13:50:21 +0200

    added components, routing, database

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
-const Header = () => {
+import Button from './Button'
+import { useLocation } from 'react-router-dom'
+
+const Header = ({ title, onAdd, showAdd }) => {
+  const onClick = () => {
+    console.log('Click');
+  }
+
+  const location = useLocation()
+
   return (
-    <div>
-      <h1>Task Tracker</h1>
-    </div>
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && (<Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>)}
+    </header>
   )
 }
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// }
 
 export default Header
