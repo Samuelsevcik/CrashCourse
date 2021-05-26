5c0a4049e5b53a91e90ba1e75d48f5a1b8a834aa
Samuel Ševčik <s.sevcik.st@spseiostrava.cz>
2021-05-26 09:23:58 +0200

    changed about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
       <h4>Version 1.0.0</h4>
+      <h5>Created by: Samuel Ševčik</h5>
+      <h5>during the intership in &reg; Unicorn</h5>
       <Link to="/">Go Back</Link>
     </div>
   )
